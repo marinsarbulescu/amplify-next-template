25ce1ea439c59bd70b90b5fc910ba96ba48d44fd
marinsarbulescu <marinsarbulescu@users.noreply.github.com>
2025-04-03 17:17:20 -0700

    API, GraphQL

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifynexttemplate": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
